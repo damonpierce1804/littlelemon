df1ea7391f71d9a469ad03543bb72a3ac05777b1
JesusN92 <7vcr8knsnc@privaterelay.appleid.com>
2026-02-04 10:50:59 -0800

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Jesus Cervantes on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
